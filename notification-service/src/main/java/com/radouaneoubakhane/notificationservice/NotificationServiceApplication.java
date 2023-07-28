f031bd708488559904ed5f4c0c8c5acda73d3746
Radouane Oubakhane <oubakhaneradouane@gmail.com>
2023-07-28 14:18:23 +0100

    Create the Notification Service

---
+++ notification-service/src/main/java/com/radouaneoubakhane/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,15 @@
+package com.radouaneoubakhane.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient // Enable discovery client
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
