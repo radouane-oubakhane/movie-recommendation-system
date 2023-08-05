102f76e3db231079f75b3419f4c501104dc77e1e
Radouane Oubakhane <oubakhaneradouane@gmail.com>
2023-08-05 15:09:01 +0100

    Create the Catalog service

---
+++ catalog-service/src/main/java/com/radouaneoubakhane/catalogservice/exception/ErrorDetails.java
@@ -0,0 +1,14 @@
+package com.radouaneoubakhane.catalogservice.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
---
+++ notification-service/src/main/java/com/radouaneoubakhane/notificationservice/util/DataLoader.java
@@ -0,0 +1,18 @@
+package com.radouaneoubakhane.notificationservice.util;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
